95df7b8d0cb5055b9470211c416d071bc7a9fd52
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-20 18:58:22 +0200

    Made algorithm of identifiyng all lines and their lengths.

--- Syntaxer/Program.cs
+++ Syntaxer/Program.cs
@@ -5,6 +5,26 @@ public class Program
     public static void Main(string[] args)
     {
         string data = File.ReadAllText("../../../Second.cs");
-        Console.WriteLine(data);
+        // Console.WriteLine(data);
+        // Console.WriteLine(data.Count(x => x == '\n'));
+        int column = 0;
+        int line = 0;
+        List<(int x, int y)> sizes = new();
+        foreach (var ch in data)
+        {
+            if (ch == '\n')
+            {
+                sizes.Add((column, line));
+                line++;
+                column = 0;
+                continue;
+            }
+            column++;
+        }
+        sizes.Add((column, line));
+        foreach (var pair in sizes)
+        {
+            Console.WriteLine($"Length: {pair.x}, Line: {pair.y}");
+        }
     }
 }
