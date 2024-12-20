ff5264ec904902c55542527a02c854d70a716941
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-20 18:32:00 +0200

    Prepared main file to work.

--- Syntaxer/Program.cs
+++ Syntaxer/Program.cs
@@ -1,9 +1,9 @@
-﻿namespace Syntaxer;
+﻿namespace Syntax;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
     }
 }
