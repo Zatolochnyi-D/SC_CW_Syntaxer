13e98475c7497eef82892b675548cc03624542f5
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-21 17:46:36 +0200

    Some tries on problem.

--- Syntaxer/ScriptFile.cs
+++ Syntaxer/ScriptFile.cs
@@ -9,6 +9,166 @@ public class ScriptFile
     public ScriptFile(string body)
     {
         this.body = body;
+        CalculateFileSize();
+    }
+
+    private void CalculateFileSize()
+    {
+        int column = 0;
+        foreach (var symbol in body)
+        {
+            if (symbol == '\n')
+            {
+                fileSize.Add(column);
+                column = 0;
+            }
+            else
+            {
+                column++;
+            }
+        }
+    }
+
+    public void ClearBody()
+    {
+        string cleanBody = "";
+        for (int i = 0; i < body.Length; i++)
+        {
+            
+        }
+    }
+
+    public void CreateBracketMap()
+    {
+        // Markers of cursor position.
+        int line = 0;
+        int column = 0;
+
+        bool isChar = false;
+        bool isString = false;
+        // Helping variables for comment detection.
+        char previousSymbol = '\0';
+        bool isLineComment = false;
+        bool isMultilineComment = false;
+
+        string brackets = "";
+
+        foreach (var symbol in body)
+        {
+            // Store two last symbols for some checks.
+            string twoSymbols = $"{previousSymbol}{symbol}";
+            previousSymbol = symbol;
+
+            if (isChar)
+            {
+                // Cursor is currently in char sequence. Detect it's end.
+                if (twoSymbols == "\'")
+                {
+                    // Special case, when char stores single quote -> '\''. Do nothing.
+                    continue;
+                }
+                if (symbol == '\'')
+                {
+                    // End of char sequence.
+                    isChar = false;
+                    continue;
+                }
+            }
+            if (isString)
+            {
+                // Cursor is currently in string sequence. Detect it's end.
+                if (twoSymbols == "\\\"")
+                {
+                    // Special case, when string stores single quote -> '\''. Do nothing.
+                    continue;
+                }
+                if (symbol == '\'')
+                {
+                    // End of char sequence.
+                    isChar = false;
+                    continue;
+                }
+            }
+
+            // Traverse through important symbols
+            if (symbol == '\n')
+            {
+                // Update cursor.
+                line++;
+                column = 0;
+                continue;
+            }
+            if (symbol == '\'')
+            {
+                // Start of char detected.
+                isChar = true;
+                continue;
+            }
+            if (symbol == '"')
+            {
+                // Start of string detected.
+                isString = true;
+                continue;
+            }
+
+
+
+            column++;
+
+            // Detect comment start.
+
+            if (twoSymbols == "//")
+            {
+                // Line comment detected.
+                isLineComment = true;
+            }
+            else if (twoSymbols == "/*")
+            {
+                // Multiline comment detected.
+                isMultilineComment = true;
+            }
+
+            // Look for end of the comment (if there is one). Do nothing else, as comment's content is ignored.
+            if (isLineComment)
+            {
+                if (symbol == '\n')
+                {
+                    // Line end is end of line comment.
+                    isLineComment = false;
+                }
+                continue;
+            }
+            if (isMultilineComment)
+            {
+                if (twoSymbols == "*/")
+                {
+                    // This sequence is end of multiline comment.
+                    isMultilineComment = false;
+                }
+                continue;
+            }
+
+            // Detect strings and chars to ignore them as well.
+
+            // Add current symbol to current member.
+            // member += symbol;
+            // if (isScanningBlock)
+            // {
+            //     // Just pass through.
+            //     continue;
+            // }
+            // if (symbol == ';')
+            // {
+            //     // End of instruction found.
+            //     members.Add(new Instruction(member));
+            //     member = "";
+            // }
+            // else if (symbol == '{')
+            // {
+            //     // Start of block found. Begin block scan.
+            //     isScanningBlock = true;
+            // }
+        }
     }
 
     public void Split()
---
+++ Syntaxer/Testing.cs
@@ -0,0 +1,13 @@
+// // One line comment
+// int val = 9; // Still one line comment
+
+// /* Multiline comment in one line */
+// /* one line comment in 
+// many lines */
+// int cal = /* Even this
+// is possible */ 9;
+
+// string str1 = "gegrege";
+// string str2 = @" sdgdfgfgd
+// gdfgdfgfg";
+// string str3 = """fsdfddg "AAAA" """;
