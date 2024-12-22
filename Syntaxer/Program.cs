c6d4a90cf51091a4bd886cd056cb105a675425e2
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-22 19:50:03 +0200

    Put other target script.

--- Syntaxer/Program.cs
+++ Syntaxer/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Syntax;
+﻿namespace Syntaxer;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        string data = File.ReadAllText("../../../Target.cs");
+        string data = File.ReadAllText("/Users/denis/Desktop/SC_CW_Syntaxer/Syntaxer/ScriptFile.cs");
         ScriptFile file = new(data);
     }
 }
