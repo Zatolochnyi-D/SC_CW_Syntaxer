b722b59f90ce396d5153f9600de1d96d7af731b8
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-22 17:29:24 +0200

    Renamed target file.

--- Syntaxer/Program.cs
+++ Syntaxer/Program.cs
@@ -4,8 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        string data = File.ReadAllText("../../../Second.cs");
+        string data = File.ReadAllText("../../../Target.cs");
         ScriptFile file = new(data);
-        file.ClearBody();
     }
 }
