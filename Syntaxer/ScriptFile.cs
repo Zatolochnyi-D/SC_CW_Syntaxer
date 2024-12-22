64d978c727a1ca6cacd38bb3502a38d99c53935b
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-22 19:50:48 +0200

    Made file separate body into members.

--- Syntaxer/ScriptFile.cs
+++ Syntaxer/ScriptFile.cs
@@ -1,257 +1,181 @@
-namespace Syntax;
+using Syntax;
+
+namespace Syntaxer;
 
 public class ScriptFile
 {
-    private readonly string body;
-    private readonly List<object> members = [];
-    private readonly List<int> fileSize = []; // Each index represents line, it's value represents length. 
+    private string originalBody;
+    private List<string> linesOfFile = [];
+    private List<int> fileSize = []; // Each index represents line, it's value represents length. 
+    private List<object> members = [];
 
     public ScriptFile(string body)
     {
-        this.body = body;
-        CalculateFileSize();
+        originalBody = body;
+        SplitFileIntoLines();
+        SplitFileIntoMembers();
     }
 
-    private void CalculateFileSize()
+    private void SplitFileIntoLines()
     {
-        int column = 0;
-        foreach (var symbol in body)
+        string line = "";
+        foreach (var symbol in originalBody)
         {
             if (symbol == '\n')
             {
-                fileSize.Add(column);
-                column = 0;
+                linesOfFile.Add(line);
+                line = "";
+                continue;
             }
-            else
-            {
-                column++;
-            }
+            line += symbol;
         }
+        linesOfFile.Add(line);
     }
 
-    public void ClearBody()
+    private void SplitFileIntoMembers()
     {
-        string cleanBody = "";
-        for (int i = 0; i < body.Length; i++)
-        {
-            
-        }
-    }
-
-    public void CreateBracketMap()
-    {
-        // Markers of cursor position.
-        int line = 0;
-        int column = 0;
-
-        bool isChar = false;
-        bool isString = false;
-        // Helping variables for comment detection.
-        char previousSymbol = '\0';
-        bool isLineComment = false;
-        bool isMultilineComment = false;
-
-        string brackets = "";
-
-        foreach (var symbol in body)
-        {
-            // Store two last symbols for some checks.
-            string twoSymbols = $"{previousSymbol}{symbol}";
-            previousSymbol = symbol;
-
-            if (isChar)
-            {
-                // Cursor is currently in char sequence. Detect it's end.
-                if (twoSymbols == "\'")
-                {
-                    // Special case, when char stores single quote -> '\''. Do nothing.
-                    continue;
-                }
-                if (symbol == '\'')
-                {
-                    // End of char sequence.
-                    isChar = false;
-                    continue;
-                }
-            }
-            if (isString)
-            {
-                // Cursor is currently in string sequence. Detect it's end.
-                if (twoSymbols == "\\\"")
-                {
-                    // Special case, when string stores single quote -> '\''. Do nothing.
-                    continue;
-                }
-                if (symbol == '\'')
-                {
-                    // End of char sequence.
-                    isChar = false;
-                    continue;
-                }
-            }
-
-            // Traverse through important symbols
-            if (symbol == '\n')
-            {
-                // Update cursor.
-                line++;
-                column = 0;
-                continue;
-            }
-            if (symbol == '\'')
-            {
-                // Start of char detected.
-                isChar = true;
-                continue;
-            }
-            if (symbol == '"')
-            {
-                // Start of string detected.
-                isString = true;
-                continue;
-            }
-
-
-
-            column++;
-
-            // Detect comment start.
-
-            if (twoSymbols == "//")
-            {
-                // Line comment detected.
-                isLineComment = true;
-            }
-            else if (twoSymbols == "/*")
-            {
-                // Multiline comment detected.
-                isMultilineComment = true;
-            }
-
-            // Look for end of the comment (if there is one). Do nothing else, as comment's content is ignored.
-            if (isLineComment)
-            {
-                if (symbol == '\n')
-                {
-                    // Line end is end of line comment.
-                    isLineComment = false;
-                }
-                continue;
-            }
-            if (isMultilineComment)
-            {
-                if (twoSymbols == "*/")
-                {
-                    // This sequence is end of multiline comment.
-                    isMultilineComment = false;
-                }
-                continue;
-            }
-
-            // Detect strings and chars to ignore them as well.
-
-            // Add current symbol to current member.
-            // member += symbol;
-            // if (isScanningBlock)
-            // {
-            //     // Just pass through.
-            //     continue;
-            // }
-            // if (symbol == ';')
-            // {
-            //     // End of instruction found.
-            //     members.Add(new Instruction(member));
-            //     member = "";
-            // }
-            // else if (symbol == '{')
-            // {
-            //     // Start of block found. Begin block scan.
-            //     isScanningBlock = true;
-            // }
-        }
-    }
-
-    public void Split()
-    {
-        // Markers of cursor position.
-        int line = 0;
-        int column = 0;
-        // Helping variables for comment detection.
-        char previousSymbol = '\0';
-        bool isLineComment = false;
-        bool isMultilineComment = false;
-        // Remember scanned text to write it as member, when one is found.
         string member = "";
-        // Helping variables for traversing blocks.
-        int bracketBalance = 0;
-        bool isScanningBlock = false;
-        // Separate body into members and calculate file size.
-        foreach (var symbol in body)
+        for (int i = 0; i < originalBody.Length; i++)
         {
-            // Count file size.
-            if (symbol == '\n')
+            if (originalBody[i] == '/')
             {
-                fileSize.Add(column);
-                line++;
-                column = 0;
-            }
-            else
-            {
-                column++;
-            }
-
-            // Detect comment start.
-            string twoSymbols = $"{previousSymbol}{symbol}";
-            if (twoSymbols == "//")
-            {
-                // Line comment detected.
-                isLineComment = true;
-            }
-            else if (twoSymbols == "/*")
-            {
-                // Multiline comment detected.
-                isMultilineComment = true;
-            }
-
-            // Look for end of the comment (if there is one). Do nothing else, as comment's content is ignored.
-            if (isLineComment)
-            {
-                if (symbol == '\n')
+                // Possible comment.
+                if (originalBody[i + 1] == '/' && i != originalBody.Length - 1)
                 {
-                    // Line end is end of line comment.
-                    isLineComment = false;
+                    // It's a comment.
+                    while (true)
+                    {
+                        // Increment i until end of line or end of file found.
+                        i++;
+                        if (i == originalBody.Length)
+                        {
+                            break;
+                        }
+                        if (originalBody[i] == '\n')
+                        {
+                            break;
+                        }
+                    }
+                    continue;
+                }
+            }
+            else if (originalBody[i] == '\'')
+            {
+                // Char declarator found.
+                member += originalBody[i];
+                while (true)
+                {
+                    // As it's a char, go forward and read it completely. It ignores structure and treats everything as content of char.
+                    i++;
+                    if (i == originalBody.Length)
+                    {
+                        // Reached end of file.
+                        break;
+                    }
+                    member += originalBody[i];
+                    if (originalBody[i] == '\'')
+                    {
+                        int j = 0;
+                        while (true)
+                        {
+                            // Go back and count amount of backslashes before char declarator.
+                            j++;
+                            if (originalBody[i - j] != '\\')
+                            {
+                                j--;
+                                break;
+                            }
+                        }
+                        if (j % 2 == 0)
+                        {
+                            // It's char end declarator.
+                            break;
+                        }
+                    }
                 }
                 continue;
             }
-            if (isMultilineComment)
+            else if (originalBody[i] == '"')
             {
-                if (twoSymbols == "*/")
+                // String declarator found.
+                member += originalBody[i];
+                while (true)
                 {
-                    // This sequence is end of multiline comment.
-                    isMultilineComment = false;
+                    // As it's a char, go forward and read it completely. It ignores structure and treats everything as content of char.
+                    i++;
+                    if (i == originalBody.Length)
+                    {
+                        // Reached end of file.
+                        break;
+                    }
+                    member += originalBody[i];
+                    if (originalBody[i] == '"')
+                    {
+                        int j = 0;
+                        while (true)
+                        {
+                            // Go back and count amount of backslashes before string declarator.
+                            j++;
+                            if (originalBody[i - j] != '\\')
+                            {
+                                j--;
+                                break;
+                            }
+                        }
+                        if (j % 2 == 0)
+                        {
+                            // It's string end declarator.
+                            break;
+                        }
+                    }
                 }
                 continue;
             }
-
-            // Add current symbol to current member.
-            member += symbol;
-            if (isScanningBlock)
-            {
-                // Just pass through.
-                continue;
-            }
-            if (symbol == ';')
+            else if (originalBody[i] == ';')
             {
                 // End of instruction found.
+                member += originalBody[i];
                 members.Add(new Instruction(member));
                 member = "";
+                continue;
             }
-            else if (symbol == '{')
+            else if (originalBody[i] == '{')
             {
-                // Start of block found. Begin block scan.
-                isScanningBlock = true;
+                // Start of block found.
+                member += originalBody[i];
+                int bracketBalance = -1;
+                while (true)
+                {
+                    i++;
+                    if (i == originalBody.Length)
+                    {
+                        // Reached end of file.
+                        break;
+                    }
+                    member += originalBody[i];
+                    if (originalBody[i] == '}')
+                    {
+                        bracketBalance++;
+                    }
+                    else if (originalBody[i] == '{')
+                    {
+                        bracketBalance--;
+                    }
+                    if (bracketBalance == 0)
+                    {
+                        break;
+                    }
+                }
+                members.Add(new Block(member));
+                member = "";
+                continue;
             }
+            member += originalBody[i];
         }
-        fileSize.Add(column);
+        
+        foreach (var m in members)
+        {
+            Console.WriteLine(m);
+        }
     }
 }
