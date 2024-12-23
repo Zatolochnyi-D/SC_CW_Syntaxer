4e7f04473750ab928c2b6a9186372d01431020cb
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-23 17:03:29 +0200

    Added proper separation of block into identifier (things before {) and content (things after {). Added recursive call into SplitContent.

--- Syntaxer/Members/Block.cs
+++ Syntaxer/Members/Block.cs
@@ -1,3 +1,5 @@
+using Syntaxer.Parsers;
+
 namespace Syntaxer.Members;
 
 /// <summary>
@@ -6,23 +8,36 @@ namespace Syntaxer.Members;
 public class Block : IMember
 {
     private string body;
-    private string content;
+    private string identifier; // part before "{" that holds info about block.
+    private string content; // part after "{" that holds info inside the block.
+    private BlockParser parser;
     private List<IMember> members = [];
 
     public Block(string body)
     {
         this.body = body;
         int indexOfOpenBracket = body.IndexOf('{');
-        content = body[indexOfOpenBracket..];
-        if (content[^1] == '}')
+        identifier = body[..indexOfOpenBracket];
+        if (indexOfOpenBracket != body.Length - 1)
         {
-            content = content[..^1];
+            // Block opening is NOT a last symbol.
+            content = body[(indexOfOpenBracket + 1)..];
+            if (content[^1] == '}') content = content[..^1];
         }
+        else
+        {
+            content = "";
+        }
+        parser = new(content);
     }
 
     public void SplitContent()
     {
-        // members = ScanUtils.ParseBody(content);
+        members = parser.ParseBody();
+        foreach (var member in members)
+        {
+            member.SplitContent();
+        }
     }
 
     public override string ToString()
