18394714c62b0a811aec23cfaea1c5005eefabc1
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-26 15:11:53 +0200

    Changed test files.

--- Syntaxer/Program.cs
+++ Syntaxer/Program.cs
@@ -6,9 +6,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        IEnumerable<string> files = Directory.EnumerateFiles("/Users/denis/Desktop/SC_things_testing", "*.cs", SearchOption.AllDirectories);
-        files = files.Where(x => !x.Contains("/obj/Debug/")).Where(x => !x.Contains("/bin/Debug/"));
-        IEnumerable<string> fileContents = files.Select(File.ReadAllText);
-        List<ScriptFile> scripts = fileContents.Select(x => new ScriptFile(x)).ToList();
+        // IEnumerable<string> files = Directory.EnumerateFiles("/Users/denis/Desktop/SC_things_testing", "*.cs", SearchOption.AllDirectories);
+        // files = files.Where(x => !x.Contains("/obj/Debug/")).Where(x => !x.Contains("/bin/Debug/"));
+        // IEnumerable<string> fileContents = files.Select(File.ReadAllText);
+        // List<ScriptFile> scripts = fileContents.Select(x => new ScriptFile(x)).ToList();
+        string data = File.ReadAllText("/Users/denis/Desktop/SC_CW_Syntaxer/Syntaxer/Members/ScriptFile.cs");
+        ScriptFile file = new(data);
+        string data2 = File.ReadAllText("/Users/denis/Desktop/SC_things_testing/Testing/Program.cs");
+        ScriptFile file2 = new(data2);
     }
 }
