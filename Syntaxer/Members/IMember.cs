24707365dbe1bc99276c83c43d4cfd088aa6cf06
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-27 18:11:21 +0200

    Added get context property for all members.

--- Syntaxer/Members/IMember.cs
+++ Syntaxer/Members/IMember.cs
@@ -1,8 +1,12 @@
+using Syntaxer.Context;
+
 namespace Syntaxer.Members;
 
 public interface IMember
 {
     public ScriptFile ParentFile { get; }
+
+    public GenericContext Context { get; }
 
     public void SplitContent();
 }
