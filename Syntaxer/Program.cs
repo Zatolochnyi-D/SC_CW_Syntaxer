3147c921dcd335ae8be06fba97db56fc37a1009c
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-28 15:26:02 +0200

    Made more flexible way to add files for scan. Made error output.

--- Syntaxer/Program.cs
+++ Syntaxer/Program.cs
@@ -10,11 +10,23 @@ public class Program
         // files = files.Where(x => !x.Contains("/obj/Debug/")).Where(x => !x.Contains("/bin/Debug/"));
         // IEnumerable<string> fileContents = files.Select(File.ReadAllText);
         // List<ScriptFile> scripts = fileContents.Select(x => new ScriptFile(x)).ToList();
-        string data = File.ReadAllText("/Users/denis/Desktop/SC_CW_Syntaxer/Syntaxer/Members/ScriptFile.cs");
-        ScriptFile file = new(data);
-        file.SplitContent();
-        string data2 = File.ReadAllText("/Users/denis/Desktop/SC_things_testing/Testing/Strings.cs");
-        ScriptFile file2 = new(data2);
-        file2.SplitContent();
+        List<string> filePathes =
+        [
+            "/Users/denis/Desktop/SC_things_testing/Testing/Strings.cs",
+            "/Users/denis/Desktop/SC_CW_Syntaxer/Syntaxer/Members/ScriptFile.cs",
+        ];
+
+        foreach (var path in filePathes)
+        {
+            Console.WriteLine($"In file {path}:");
+            string data = File.ReadAllText(path);
+            ScriptFile file = new(data);
+            file.SplitContent();
+            foreach (var exception in file.ExceptionsAsStrings())
+            {
+                Console.Write("  ");
+                Console.WriteLine(exception);
+            }
+        }
     }
 }
