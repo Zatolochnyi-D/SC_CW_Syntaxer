7e84816026754438cba050953e32e148e567cfb0
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-28 14:41:39 +0200

    Added store of dimension of string literal.

--- Syntaxer/Literals/StringLiteral.cs
+++ Syntaxer/Literals/StringLiteral.cs
@@ -3,9 +3,11 @@ namespace Syntaxer.Literals;
 public class StringLiteral
 {
     private string body;
+    private (int begin, int end) dimension;
 
-    public StringLiteral(string stringContent)
+    public StringLiteral(string stringContent, (int, int) dimension)
     {
         body = stringContent;
+        this.dimension = dimension;
     }
 }
