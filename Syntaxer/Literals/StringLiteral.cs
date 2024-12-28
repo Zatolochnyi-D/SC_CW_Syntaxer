170563876e5d3eee08f766ac1cecb54a77134393
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-28 15:15:22 +0200

    Added a comment about body specifics.

--- Syntaxer/Literals/StringLiteral.cs
+++ Syntaxer/Literals/StringLiteral.cs
@@ -2,7 +2,7 @@ namespace Syntaxer.Literals;
 
 public class StringLiteral
 {
-    private string body;
+    private string body; // Due to nature of StringParser, this string will always start and end with " or '.
     private (int begin, int end) dimension;
 
     public StringLiteral(string stringContent, (int, int) dimension)
