130193673eef34cc0c34b06b3db011cac98710ec
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-29 20:10:22 +0200

    Added operators list to keywords collection.

--- Syntaxer/Keywords.cs
+++ Syntaxer/Keywords.cs
@@ -3,4 +3,32 @@ namespace Syntaxer;
 public static class Keywords
 {
     public const string NAMESPACE = "namespace";
+
+    public static readonly string[] OPERATORS =
+    [
+        "+",
+        "-",
+        "*",
+        "/",
+        "%",
+        "&",
+        "&&",
+        "|",
+        "||",
+        "==",
+        "!=",
+        "<",
+        ">",
+        "<=",
+        ">=",
+        "=",
+        "+=",
+        "-=",
+        "*=",
+        "/=",
+        "%=",
+        "++",
+        "--",
+        "=>",
+    ];
 }
