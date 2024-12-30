8890934b972094d8e363a7c41fed1cb7717f8a81
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-30 20:34:35 +0200

    Added access modifiers to Keywords.

--- Syntaxer/Keywords.cs
+++ Syntaxer/Keywords.cs
@@ -4,6 +4,19 @@ public static class Keywords
 {
     public const string NAMESPACE = "namespace";
 
+    public const string PRIVATE = "private";
+    public const string PROTECTED = "protected";
+    public const string PUBLIC = "public";
+    public const string INTERNAL = "internal";
+
+    public static readonly string[] ACCESS_MODIFIERS =
+    [
+        PRIVATE,
+        PROTECTED,
+        PUBLIC,
+        INTERNAL
+    ];
+
     public static readonly string[] ACCESS_OPERATORS =
     [
         ".",
