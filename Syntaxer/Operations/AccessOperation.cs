70797b23838101b5f499c16b4b3840a5c57e5ac6
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-31 22:54:39 +0200

    Made access operation use name validator.

--- Syntaxer/Operations/AccessOperation.cs
+++ Syntaxer/Operations/AccessOperation.cs
@@ -1,3 +1,6 @@
+using Syntaxer.Exceptions;
+using Syntaxer.Validators;
+
 namespace Syntaxer.Operations;
 
 public class AccessOperation
@@ -5,11 +8,16 @@ public class AccessOperation
     private List<string> parts = [];
     private int leftEndIndex = 0;
     private int rightEndIndex = 0;
+    private int position;
+    private List<SyntaxException> exceptions = [];
 
-    public AccessOperation(params string[] parts)
+    public List<SyntaxException> Exceptions => exceptions;
+
+    public AccessOperation(int position, params string[] parts)
     {
         this.parts.AddRange(parts);
         rightEndIndex = parts.Length - 1;
+        this.position = position;
     }
 
     public string GetLeft()
@@ -26,16 +34,28 @@ public class AccessOperation
     {
         if (GetRight() == b.GetLeft())
         {
-            return new AccessOperation(parts.Concat(b.parts.Skip(1)).ToArray());
+            return new AccessOperation(position, parts.Concat(b.parts.Skip(1)).ToArray());
         }
         else if (GetLeft() == b.GetRight())
         {
-            return new AccessOperation(b.parts.Concat(parts.Skip(1)).ToArray());
+            return new AccessOperation(b.position, b.parts.Concat(parts.Skip(1)).ToArray());
         }
         else
         {
             return null;
         }
+    }
+
+    public void Validate()
+    {
+        if (parts.Contains(""))
+        {
+            // There is improperly located dots.
+            exceptions.Add(new LongWordException(position, LongWordException.WORD_INCOMPLETE_MESSAGE));
+        }
+        var validator = new NameValidator(position, [.. parts]);
+        validator.Validate();
+        exceptions.AddRange(validator.Exceptions);
     }
 
     public override string ToString()
