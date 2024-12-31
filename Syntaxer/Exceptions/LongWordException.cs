6619e0bf788e1ed33b7284a936b6917b6304759e
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-31 22:51:51 +0200

    Added few error messages to long word exception.

--- Syntaxer/Exceptions/LongWordException.cs
+++ Syntaxer/Exceptions/LongWordException.cs
@@ -2,7 +2,12 @@ namespace Syntaxer.Exceptions;
 
 public class LongWordException : SyntaxException
 {
+    private const string KEYWORD_USED_MESSAGE = "Keyword \"{0}\" was found in word.";
+    private const string INVALID_SYMBOLS_MESSAGE = "Name {0} does not contain any valid symbols.";
     public const string WORD_INCOMPLETE_MESSAGE = "One ore more words are missing in long word. Check on dots placement.";
 
     public LongWordException(int index, string message) : base(index, message) { }
+
+    public static string GetKeywordFoundMessage(string keyword) => string.Format(KEYWORD_USED_MESSAGE, keyword);
+    public static string GetInvalidSymbolsMessage(string symbols) => string.Format(INVALID_SYMBOLS_MESSAGE, symbols);
 }
