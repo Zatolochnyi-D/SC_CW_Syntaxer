ff8f418a5fe2938ea1c955d4886d090874429af1
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2025-01-01 15:34:56 +0200

    Added more modifer exception messages.

--- Syntaxer/Exceptions/ModifierException.cs
+++ Syntaxer/Exceptions/ModifierException.cs
@@ -2,13 +2,15 @@ namespace Syntaxer.Exceptions;
 
 public class ModifierException : SyntaxException
 {
-    private const string UNRECOGNIZED_MODIFIER_MESSAGE = "Keyword \"{0}\" is not recognized.";
+    private const string NON_MODIFIER_MESSAGE = "Word \"{0}\" is used like modifier, when it's not one.";
+    private const string WRONG_CONTEXT_MESSAGE = "Keyword \"{0}\" is not allowed in current context.";
     private const string DUPLICATE_MODIFIER_MESSAGE = "Keyword \"{0}\" has duplicate(s).";
     private const string CONFLICTED_MODIFIERS_MESSAGE = "Keywords {0} cannot be used together.";
 
     public ModifierException(int index, string message) : base(index, message) { }
 
-    public static string GetUnrecognizedWordMessage(string word) => string.Format(UNRECOGNIZED_MODIFIER_MESSAGE, word);
+    public static string GetNonModifierMessage(string word) => string.Format(NON_MODIFIER_MESSAGE, word);
+    public static string GetWrongContextMessage(string word) => string.Format(WRONG_CONTEXT_MESSAGE, word);
     public static string GetDuplicateWordMessage(string word) => string.Format(DUPLICATE_MODIFIER_MESSAGE, word);
-    public static string GetConflictedMessage(List<string> words) => string.Format(CONFLICTED_MODIFIERS_MESSAGE, $"\"{string.Join("\", \"", words)}\"");
+    public static string GetConflictedMessage(IEnumerable<string> words) => string.Format(CONFLICTED_MODIFIERS_MESSAGE, $"\"{string.Join("\", \"", words)}\"");
 }
